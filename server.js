efdf414454cfbe1f68acb2bcc7ed3b2ccc182753
ipz06 <it.vania.9@gmail.com>
2025-02-09 16:05:01 +0200

    Connect with MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 //routes
@@ -10,4 +12,14 @@ app.get('/blog', (req, res) => {
     res.send('Hello World YEEEEE')
   })
 
-app.listen(3000)
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(3000, () => {
+        console.log('Node Api is running on port 3000')
+    })
+    console.log('Connected to MongoDb')
+}).catch((err) => {
+    console.log(err)
+})
